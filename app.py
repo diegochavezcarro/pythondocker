57441f50f83c8def93a6a123c75ee3ca7ccf9dad
Diego Chavez <diegochavezcarro@gmail.com>
2022-11-21 14:16:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def hello():
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', debug=True)
